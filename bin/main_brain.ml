b6effc58ed1ddefddd8abdaa6245da7aea9c188b
munga <mwachilumobm@gmail.com>
2024-05-16 16:15:10 +0300

    Run the interpreter

---
+++ bin/main_brain.ml
@@ -0,0 +1,22 @@
+(* bin/main.ml *)
+
+open BrainCaml
+
+let print_usage (filename : string) : unit =
+  let usage_msg = "Usage: " ^ filename ^ " <filename>" in
+  print_endline usage_msg
+
+let run_interpreter (filename : string) : unit =
+  try
+    let ic = open_in filename in
+    let program = really_input_string ic (in_channel_length ic) in
+    close_in ic ; execute_program program
+  with
+  | Sys_error err ->
+      print_endline ("Error: " ^ err)
+  | End_of_file ->
+      ()
+
+let _ =
+  if Array.length Sys.argv <> 2 then print_usage Sys.argv.(0)
+  else run_interpreter Sys.argv.(1)
